d01ab153246406ec15c58ddb98b9af3624694d7d
Filipe Utzig <filipeutzig@gmail.com>
2023-05-02 21:15:14 -0300

    Improve requests logging
    Signed-off-by: Filipe Utzig <filipeutzig@gmail.com>

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
 	"github.com/gringolito/pi-hole-manager/router"
 )
 
@@ -11,12 +13,17 @@ const STATIC_HOSTS_FILE_PATH string = "./04-pihole-static-dhcp.conf"
 
 func main() {
 	app := fiber.New(fiber.Config{
-		// Prefork:       true,
+		// Prefork:           true,
 		// StrictRouting:     true,
 		CaseSensitive:     true,
 		EnablePrintRoutes: true,
 		AppName:           "Pi-Hole Manager v0.1.0",
 	})
+
+	app.Use(requestid.New())
+	app.Use(logger.New(logger.Config{
+		Format: "${pid} ${locals:requestid} [${ip}]:${port} ${status} - ${method} ${path}​\n",
+	}))
 
 	router.SetupRoutes(app, STATIC_HOSTS_FILE_PATH)
 
